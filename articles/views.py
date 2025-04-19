57d8b9e9a5b89e2883f946d85aae1324487df619
Ranir-hub <arina2005vvv@gmail.com>
2025-04-19 20:38:34 +0500

    lab9

--- articles/views.py
+++ articles/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
+from django.contrib.auth.decorators import login_required
 from articles.models import Article
-
+from . import forms
 
 def article_list(request):
     articles = Article.objects.all().order_by('date')
@@ -10,3 +11,16 @@ def article_item(request, slug):
     articles = Article.objects.all().order_by('date')
     article = Article.objects.get(slug=slug)
     return render(request, 'articles/article_item.html', {'article': article, 'articles' : articles})
+
+@login_required(login_url = 'accounts:login')
+def article_create(request):
+    if request.method == 'POST':
+        form = forms.CreateArticle(request.POST, request.FILES)
+        if form.is_valid():
+            instance = form.save(commit = False)
+            instance.author = request.user
+            instance.save()
+            return redirect('homepage')
+    else:
+        form = forms.CreateArticle()
+    return render(request, 'articles/article_create.html', {'form': form})
